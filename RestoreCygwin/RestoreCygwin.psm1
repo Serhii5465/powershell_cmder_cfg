447282bdbdcf72d331c61a361397e31c88312a53
Serhii5465 <serhiiartiukh5@gmail.com>
2024-02-12 17:50:54 +0200

    Added waiting for the installation process to complete

--- RestoreCygwin/RestoreCygwin.psm1
+++ RestoreCygwin/RestoreCygwin.psm1
@@ -1,9 +1,11 @@
+Set-Location $PSScriptRoot
+
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SillyContinue'
 
 function RestoreCygwin {
-    $File_Setup = "${PSScriptRoot}\setup.exe"
-    $File_Packages = "${PSScriptRoot}\list_packages.dat"
+    $File_Setup = "setup.exe"
+    $File_Packages = "list_packages.dat"
     $URI_Cygwin = "https://www.cygwin.com/setup-x86_64.exe"
     $Root_Install = "D:\system\applications\cygwin64"
 
@@ -21,7 +23,20 @@ function RestoreCygwin {
 
     $Packages = Get-Content -Path $File_Packages
 
-    Start-Process -FilePath "setup-x86_64.exe" -ArgumentList "--quiet-mode --site https://sunsite.icm.edu.pl/pub/cygnus/cygwin/ -R $Root_Install --no-startmenu --no-desktop --arch x86_64 -P $Packages"
+    $Proc = Start-Process -PassThru -FilePath $File_Setup -ArgumentList "--quiet-mode --site https://sunsite.icm.edu.pl/pub/cygnus/cygwin/ -R $Root_Install --no-startmenu --no-desktop --arch x86_64 -P $Packages"
+    $handle = $proc.Handle
+    $Proc.WaitForExit();
+
+    if($Proc.ExitCode -eq 0){
+        Write-Host "Removing temporary files"
+
+        Remove-Item -Recurse -Force 'https*'
+        Remove-Item -Force $File_Setup
+
+        Write-Host "Succes.Exit with status code $($Proc.ExitCode)"
+    } else {
+        Write-Warning "Exit with status code $($Proc.ExitCode)"
+    }
 }
 
 Export-ModuleMember -Function RestoreCygwin
