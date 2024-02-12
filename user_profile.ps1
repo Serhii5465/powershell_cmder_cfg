cfbc5f0b86d17421c92255666f8dadeca5afd784
Serhii5465 <serhiiartiukh5@gmail.com>
2024-02-12 17:09:21 +0200

    user_profile.ps1 init

---
+++ user_profile.ps1
@@ -0,0 +1,14 @@
+cd $env:userprofile
+
+function Global:prompt{
+    $curdir = $ExecutionContext.SessionState.Path.CurrentLocation.Path.Split("\")[-1]
+
+    if($curdir.Length -eq 0) {
+        $curdir = $ExecutionContext.SessionState.Drive.Current.Name+":\"
+    }
+    
+    Write-Host "" 
+    Write-Host ("PS " + $env:USERNAME + ":") -ForegroundColor Green -NoNewline
+    Write-Host ("[" + $curdir + "]") -ForegroundColor DarkYellow
+    return "-> "
+}
